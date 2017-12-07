fd7ff4e3f8dd1119887042d1841374396dd84001
latishasumardy <sumardy5031@mydusd.org>
2017-12-07 10:05:31 -0800

    2.1.5

---
+++ app/src/main/java/com/example/latisha/collegeapp/FamilyMember.java
@@ -0,0 +1,27 @@
+package com.example.latisha.collegeapp;
+
+/**
+ * Created by Latisha on 12/7/17.
+ */
+
+public class FamilyMember {
+    //step 5 implemented in lines 9-10
+    private String firstName;
+    private String lastName;
+    //step 6 implemented in lines 11-25
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
