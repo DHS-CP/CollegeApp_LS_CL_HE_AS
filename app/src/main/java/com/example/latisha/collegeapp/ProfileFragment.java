b2b781e278f315ac9b445857162ec7064f76f322
Camryn Lee <31013536+camrynl@users.noreply.github.com>
2017-12-11 18:50:49 -0800

    step 19

--- app/src/main/java/com/example/latisha/collegeapp/ProfileFragment.java
+++ app/src/main/java/com/example/latisha/collegeapp/ProfileFragment.java
@@ -8,35 +8,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+
 
 /**
  * Created by Latisha on 12/5/17.
  */
 //2.1.4 step 28 is this whole file
-public class ProfileFragment extends Fragment {
-    //2.1.5 step 13 implemented in lines 17-22 below
-    TextView mFirstNameText;
-    EditText mFirstNameEdit;
-    TextView mLastNameText;
-    EditText mLastNameEdit;
-
-    Profile mPerson =new Profile();
-
-    @Override
     public View onCreateView(LayoutInflater one, ViewGroup two , Bundle three) {
         super.onCreateView(one, two, three);
         View rootView = one.inflate(R.layout.fragment_profile, two, false);
 
-        //2.1.5 step 13 in lines 29-36 below
-        TextView mFirstNameText = rootView.findViewById(R.id.FirstNameText);
-        EditText mFirstNameEdit;
-        TextView mLastNameText = rootView.findViewById(R.id.LastNameText);
-        EditText mLastNameEdit;
+        //2.1.5 step 13 in lines 31-33 below
+        final TextView mFirstNameText = rootView.findViewById(R.id.FirstNameText);
+        final TextView mLastNameText = rootView.findViewById(R.id.LastNameText);
 
+        //2.1.5 step 19 implemented in lines 36-54 below
+        final EditText mFirstNameEdit = rootView.findViewById(R.id.FirstNameEdit);
+        final EditText mLastNameEdit = rootView.findViewById(R.id.LastNameEdit);
+
+        Button mSumbitButton = (Button) rootView.findViewById(R.id.mSubmitButton);
 
         mFirstNameText.setText(mPerson.getFirstName());
         mLastNameText.setText(mPerson.getLastName());
+        mFirstNameEdit.setText(mPerson.getFirstName());
+        mLastNameEdit.setText(mPerson.getLastName());
 
+        mSumbitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPerson.setFirstName(mFirstNameEdit.getText().toString());
+                mPerson.setLastName(mLastNameEdit.getText().toString());
+                mFirstNameText.setText(mPerson.getFirstName());
+                mLastNameText.setText(mPerson.getLastName());
+            }
+        });
         return rootView;
     }
 }
