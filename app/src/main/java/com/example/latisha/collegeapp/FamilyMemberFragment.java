1ea3b89dda0bb525c2ee7afa5d4aab5579d5a5be
Camryn Lee <31013536+camrynl@users.noreply.github.com>
2017-12-11 18:06:06 -0800

    2.1.5 step 11

--- app/src/main/java/com/example/latisha/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/com/example/latisha/collegeapp/FamilyMemberFragment.java
@@ -15,18 +15,31 @@ import android.widget.TextView;
 public class FamilyMemberFragment extends Fragment {
     //2.1.5 step 7 in lines 17-20
     TextView mFirstNameText;
-    EditText eFirstNameEdit;
+    EditText mFirstNameEdit;
     TextView mLastNameText;
     EditText mLastNameEdit;
 
     //2.1.5 step 9 in line 23
     FamilyMember mMember=new FamilyMember();
 
-    //2.1.4 steps 23-26 implemented in lines 16-20
+
+    //2.1.4 steps 23-26 implemented in lines 30-34
     @Override
     public View onCreateView(LayoutInflater one, ViewGroup two , Bundle three) {
         super.onCreateView(one, two, three);
         View rootView = one.inflate(R.layout.fragment_family_member, two, false);
+
+
+        //2.1.5 step 11 in lines 34-41 below
+        TextView mFirstNameText = rootView.findViewById(R.id.FirstNameText);
+        EditText mFirstNameEdit;
+        TextView mLastNameText = rootView.findViewById(R.id.LastNameText);
+        EditText mLastNameEdit;
+
+        
+        mFirstNameText.setText(mMember.getFirstName());
+        mLastNameText.setText(mMember.getLastName());
+
         return rootView;
     }
 }
