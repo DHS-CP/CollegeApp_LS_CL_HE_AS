95334b197b76db0bbcd17127f44e149d7aabf707
Camryn Lee <31013536+camrynl@users.noreply.github.com>
2017-12-11 18:19:35 -0800

    step 13

--- app/src/main/java/com/example/latisha/collegeapp/ProfileFragment.java
+++ app/src/main/java/com/example/latisha/collegeapp/ProfileFragment.java
@@ -11,10 +11,29 @@ import android.view.ViewGroup;
  */
 //2.1.4 step 28 is this whole file
 public class ProfileFragment extends Fragment {
+    //2.1.5 step 13 implemented in lines 17-22 below
+    TextView mFirstNameText;
+    EditText mFirstNameEdit;
+    TextView mLastNameText;
+    EditText mLastNameEdit;
+
+    Profile mPerson =new Profile();
+
     @Override
     public View onCreateView(LayoutInflater one, ViewGroup two , Bundle three) {
         super.onCreateView(one, two, three);
         View rootView = one.inflate(R.layout.fragment_profile, two, false);
+
+        //2.1.5 step 13 in lines 29-36 below
+        TextView mFirstNameText = rootView.findViewById(R.id.FirstNameText);
+        EditText mFirstNameEdit;
+        TextView mLastNameText = rootView.findViewById(R.id.LastNameText);
+        EditText mLastNameEdit;
+
+
+        mFirstNameText.setText(mPerson.getFirstName());
+        mLastNameText.setText(mPerson.getLastName());
+
         return rootView;
     }
 }
